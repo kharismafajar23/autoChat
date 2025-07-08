155bbe8b7617f42a11342514dc0b977cfc58427b
Kharisma Fajar <restuistyo@gmail.com>
2025-07-08 16:18:54 +0700

    Hasilnya

---
+++ assets/scripts/main.js
@@ -0,0 +1,31 @@
+const generateButton = document.querySelector("#generate_button");
+const copyButton = document.querySelector("#copy_button");
+
+generateButton.addEventListener("click", function () {
+  let namaPengirim = document.querySelector("#nama_pengirim").value;
+  let namaPenerima = document.querySelector("#nama_penerima").value;
+  let noTelp = document.querySelector("#input_telepon").value;
+  let sebutan = document.querySelector("#input_sebutan").value;
+
+  let templatePesan = document.querySelector("#input_pesan").value;
+  let hasil = templatePesan
+    .replace(/\[NamaPengirim\]/g, namaPengirim)
+    .replace(/\[NamaPenerima\]/g, namaPenerima)
+    .replace(/\[Sebutan\]/g, sebutan);
+
+  document.querySelector(
+    "#telepon_tujuan"
+  ).innerText = `http://wa.me/+62${noTelp}`;
+  document.querySelector("#text_hasil").innerText = hasil;
+
+  // Kosongkan input setelah submit
+  document.querySelector("#nama_pengirim").value = "";
+  document.querySelector("#nama_penerima").value = "";
+  document.querySelector("#input_sebutan").value = "";
+});
+
+copyButton.addEventListener("click", function () {
+  const hasilTeks = document.querySelector("#text_hasil").innerText;
+
+  navigator.clipboard.writeText(hasilTeks);
+});
